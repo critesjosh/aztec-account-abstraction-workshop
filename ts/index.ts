7cf18adb09fca4bb16e23760298be2ea8272e8cf
critesjosh <jc@joshcrites.com>
2023-10-31 21:38:40 -0400

    simple blank contract interaction

--- ts/index.ts
+++ ts/index.ts
@@ -1,60 +1,30 @@
-import {
-  CounterContractArtifact,
-  CounterContract,
-} from "../contracts/artifacts/Counter.js";
-import {
-  Fr,
-  createPXEClient,
-  generatePublicKey,
-  GrumpkinScalar,
-  SignerlessWallet,
-  computeMessageSecretHash,
-  Note,
-  ExtendedNote,
-} from "@aztec/aztec.js";
-import { getContractDeploymentInfo } from "@aztec/circuits.js";
+import { BlankContract } from "../contracts/artifacts/Blank.js";
+import { Fr, createPXEClient, SignerlessWallet } from "@aztec/aztec.js";
 
 async function main() {
-  const secret = new Fr(100);
   const salt = Fr.random();
-  const encryptionKey = GrumpkinScalar.random();
-  const publicKey = await generatePublicKey(encryptionKey);
 
   const pxe = createPXEClient("http://localhost:8080");
   const nonContractAccountWallet = new SignerlessWallet(pxe);
 
-  const deploymentInfo = await getContractDeploymentInfo(
-    CounterContractArtifact,
-    [secret],
-    salt,
-    publicKey
-  );
-
-  await pxe.registerAccount(
-    encryptionKey,
-    deploymentInfo.completeAddress.partialAddress
-  );
-
-  const zeroAddress = Buffer.from(new Uint8Array(32).fill(0));
-
-  const tx = await CounterContract.deployWithPublicKey(
-    publicKey,
-    nonContractAccountWallet,
-    1n,
-    zeroAddress
-  ).send({ contractAddressSalt: salt });
+  const tx = await BlankContract.deploy(nonContractAccountWallet).send({
+    contractAddressSalt: salt,
+  });
   const contract = await tx.deployed();
   const receipt = await tx.getReceipt();
 
-  console.log("deployed");
+  console.log(receipt);
 
-  if (contract.address) {
-    // const secretHash = await computeMessageSecretHash(secret);
-    // const note = new Note([secretHash]);
-    // const extendedNote = new ExtendedNote(note, contract.address, contract.address, new Fr(1), receipt.txHash);
-    // await pxe.addNote(extendedNote);
-    return contract.address;
-  }
+  let interactionTx = await contract.methods
+    .getPublicKey(
+      BigInt(
+        "0x15701f0b6cf4d273aa0b4ecf2abbb028c1c0286ba7309a2f20b253f297e1908e"
+      )
+    )
+    .send()
+    .wait();
+
+  console.log("interaction tx", interactionTx);
 }
 
 main();
