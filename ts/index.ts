76a7284d6af361648852f96c23c88fc688d2e23f
critesjosh <jc@joshcrites.com>
2023-10-31 21:59:27 -0400

    signerless deploy and interact

--- ts/index.ts
+++ ts/index.ts
@@ -1,21 +1,13 @@
-import {
-  CounterContractArtifact,
-  CounterContract,
-} from "../contracts/artifacts/Counter.js";
+import { CounterContract } from "../contracts/artifacts/Counter.js";
 import {
   Fr,
   createPXEClient,
   generatePublicKey,
   GrumpkinScalar,
   SignerlessWallet,
-  computeMessageSecretHash,
-  Note,
-  ExtendedNote,
 } from "@aztec/aztec.js";
-import { getContractDeploymentInfo } from "@aztec/circuits.js";
 
 async function main() {
-  const secret = new Fr(100);
   const salt = Fr.random();
   const encryptionKey = GrumpkinScalar.random();
   const publicKey = await generatePublicKey(encryptionKey);
@@ -23,38 +15,35 @@ async function main() {
   const pxe = createPXEClient("http://localhost:8080");
   const nonContractAccountWallet = new SignerlessWallet(pxe);
 
-  const deploymentInfo = await getContractDeploymentInfo(
-    CounterContractArtifact,
-    [secret],
-    salt,
-    publicKey
-  );
+  const headstart = 2n;
 
-  await pxe.registerAccount(
-    encryptionKey,
-    deploymentInfo.completeAddress.partialAddress
-  );
-
-  const zeroAddress = Buffer.from(new Uint8Array(32).fill(0));
+  const completeAddress = await pxe.registerAccount(encryptionKey, Fr.ZERO);
+  console.log("completeAddress", completeAddress);
 
   const tx = await CounterContract.deployWithPublicKey(
     publicKey,
     nonContractAccountWallet,
-    1n,
-    zeroAddress
+    headstart,
+    completeAddress.address
   ).send({ contractAddressSalt: salt });
   const contract = await tx.deployed();
   const receipt = await tx.getReceipt();
 
   console.log("deployed");
 
-  if (contract.address) {
-    // const secretHash = await computeMessageSecretHash(secret);
-    // const note = new Note([secretHash]);
-    // const extendedNote = new ExtendedNote(note, contract.address, contract.address, new Fr(1), receipt.txHash);
-    // await pxe.addNote(extendedNote);
-    return contract.address;
-  }
+  let count = await contract.methods
+    .get_counter(completeAddress.address)
+    .view();
+
+  console.log("count", count);
+
+  await contract.methods.increment(completeAddress.address).send().wait();
+
+  let newCount = await contract.methods
+    .get_counter(completeAddress.address)
+    .view();
+
+  console.log("new count", newCount);
 }
 
 main();
